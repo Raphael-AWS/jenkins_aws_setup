391300686e9c49c2c89753c9388870300552f727
Raphael Titus <56334419+Raphael-AWS@users.noreply.github.com>
2019-10-24 12:52:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,140 +10,126 @@ terraform {
 provider "aws" {
   region = "${var.aws_region}"
 }
-# lookup for the "default" VPC
-data "aws_vpc" "default_vpc" {
-  default = true
-}
-
-# subnet list in the "default" VPC
-# The "default" VPC has all "public subnets"
-data "aws_subnet_ids" "default_public" {
-  vpc_id = "${data.aws_vpc.default_vpc.id}"
-}
-# Security Group:
-resource "aws_security_group" "jenkins_server" {
-  name        = "jenkins_server"
-  description = "Jenkins Server: created by Terraform for [dev]"
-
-  # legacy name of VPC ID
-  vpc_id = "${data.aws_vpc.default_vpc.id}"
-
-  tags {
-    Name = "jenkins_server"
-    env  = "dev"
-  }
-}
-
-###############################################################################
-# ALL INBOUND
-###############################################################################
-
-# ssh
-resource "aws_security_group_rule" "jenkins_server_from_source_ingress_ssh" {
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  security_group_id = "${aws_security_group.jenkins_server.id}"
-  cidr_blocks       = ["<Your Public IP>/32", "172.0.0.0/8"]
-  description       = "ssh to jenkins_server"
-}
-
-# web
-resource "aws_security_group_rule" "jenkins_server_from_source_ingress_webui" {
-  type              = "ingress"
-  from_port         = 8080
-  to_port           = 8080
-  protocol          = "tcp"
-  security_group_id = "${aws_security_group.jenkins_server.id}"
-  cidr_blocks       = ["0.0.0.0/0"]
-  description       = "jenkins server web"
-}
-
-# JNLP
-resource "aws_security_group_rule" "jenkins_server_from_source_ingress_jnlp" {
-  type              = "ingress"
-  from_port         = 33453
-  to_port           = 33453
-  protocol          = "tcp"
-  security_group_id = "${aws_security_group.jenkins_server.id}"
-  cidr_blocks       = ["172.31.0.0/16"]
-  description       = "jenkins server JNLP Connection"
-}
-
-###############################################################################
-# ALL OUTBOUND
-###############################################################################
-
-resource "aws_security_group_rule" "jenkins_server_to_other_machines_ssh" {
-  type              = "egress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  security_group_id = "${aws_security_group.jenkins_server.id}"
-  cidr_blocks       = ["0.0.0.0/0"]
-  description       = "allow jenkins servers to ssh to other machines"
-}
-
-resource "aws_security_group_rule" "jenkins_server_outbound_all_80" {
-  type              = "egress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  security_group_id = "${aws_security_group.jenkins_server.id}"
-  cidr_blocks       = ["0.0.0.0/0"]
-  description       = "allow jenkins servers for outbound yum"
-}
-
-resource "aws_security_group_rule" "jenkins_server_outbound_all_443" {
-  type              = "egress"
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
-  security_group_id = "${aws_security_group.jenkins_server.id}"
-  cidr_blocks       = ["0.0.0.0/0"]
-  description       = "allow jenkins servers for outbound yum"
-}
-# lookup the security group of the Jenkins Server
-data "aws_security_group" "jenkins_server" {
-  filter {
-    name   = "group-name"
-    values = ["jenkins_server"]
-  }
-}
-
-# userdata for the Jenkins server ...
-data "template_file" "jenkins_server" {
-  template = "${file("scripts/jenkins_server.sh")}"
-
-  vars {
-    env = "dev"
-    jenkins_admin_password = "mysupersecretpassword"
-  }
-}
-
-# the Jenkins server itself
-resource "aws_instance" "jenkins_server" {
-  ami                       = "${lookup(var.amis,var.aws_region)}"
-  instance_type          		= "t3.medium"
-  key_name                  = "${var.aws_key_name}"
-  subnet_id              		= "${data.aws_subnet_ids.default_public.ids[0]}"
-  vpc_security_group_ids 		= ["${data.aws_security_group.jenkins_server.id}"]
-  iam_instance_profile   		= "dev_jenkins_server"
-  user_data                 = "${file("userdata.sh")}"
-
-  tags {
-    "Name" = "jenkins_server"
-  }
-
+## Master jenkins instance
+resource "aws_instance" "jenkins_master" {
+  ami               = "${lookup(var.amis,var.aws_region)}"
+  instance_type          = "${var.instance_type}"
+  key_name               = "${var.key_name}"
+  vpc_security_group_ids = ["${var.security_group_id}"]
+  subnet_id              = "${var.subnet_id}"
   root_block_device {
-    delete_on_termination = true
+    volume_type           = "gp2"
+    volume_size           = 30
+    delete_on_termination = false
   }
-}
-output "jenkins_server_public_ip" {
-  value = "${aws_instance.jenkins_server.public_ip}"
+
 }
 
-output "jenkins_server_private_ip" {
-  value = "${aws_instance.jenkins_server.private_ip}"
+
+// Jenkins slaves launch configuration
+resource "aws_launch_configuration" "jenkins_slave_launch_conf" {
+  name            = "jenkins_slaves_config"
+  image_id        = "${lookup(var.amis,var.aws_region)}"
+  instance_type   = "${var.instance_type}"
+  key_name        = "${var.key_name}"
+  security_groups = ["${var.security_group_id}"]
+  #user_data       = "${data.template_file.user_data_slave.rendered}"
+   user_data       = "${file("userdata.sh")}"
+root_block_device {
+    volume_type           = "gp2"
+    volume_size           = 30
+    delete_on_termination = false
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+// ASG Jenkins slaves
+resource "aws_autoscaling_group" "jenkins_slaves" {
+  name                 = "jenkins_slaves_asg"
+  launch_configuration = "${aws_launch_configuration.jenkins_slave_launch_conf.name}"
+  vpc_zone_identifier  = ["${var.subnet_id}"]
+  max_size             = "${var.asg_max}"
+  min_size             = "${var.asg_min}"
+  depends_on = ["aws_instance.jenkins_master"]
+  lifecycle {
+    create_before_destroy = true
+  }
+  tag {
+    key                 = "Name"
+    value               = "jenkins_slave"
+    propagate_at_launch = true
+  }
+  tag {
+    key                 = "Author"
+    value               = "coe-wipro"
+    propagate_at_launch = true
+  }
+  tag {
+    key                 = "Tool"
+    value               = "Terraform"
+    propagate_at_launch = true
+  }
+}
+
+// Scale out
+resource "aws_cloudwatch_metric_alarm" "high-cpu-jenkins-slaves-alarm" {
+  alarm_name          = "high-cpu-jenkins-slaves-alarm"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "120"
+  statistic           = "Average"
+  threshold           = "80"
+  alarm_description = "This metric monitors ec2 cpu utilization"
+  alarm_actions     = ["${aws_autoscaling_policy.scale-out.arn}"]
+}
+resource "aws_autoscaling_policy" "scale-out" {
+  name                   = "scale-out-jenkins-slaves"
+  scaling_adjustment     = 1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 300
+  autoscaling_group_name = "${aws_autoscaling_group.jenkins_slaves.name}"
+}
+// Scale In
+resource "aws_cloudwatch_metric_alarm" "low-cpu-jenkins-slaves-alarm" {
+  alarm_name          = "low-cpu-jenkins-slaves-alarm"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "120"
+  statistic           = "Average"
+  threshold           = "20"
+  alarm_description = "This metric monitors ec2 cpu utilization"
+  alarm_actions     = ["${aws_autoscaling_policy.scale-in.arn}"]
+}
+resource "aws_autoscaling_policy" "scale-in" {
+  name                   = "scale-in-jenkins-slaves"
+  scaling_adjustment     = -1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 300
+  autoscaling_group_name = "${aws_autoscaling_group.jenkins_slaves.name}"
+}
+resource "aws_elb" "jenkins" {
+  name = "jenkins-elb"
+  security_groups = ["${var.security_group_id}"]
+  #availability_zones   = ["${split(",", var.aws_availability_zones)}"]
+#availability_zones = ["us-east-1a"]
+#vpc_zone_identifier = ["${aws_subnet.public_subnet_us-east-1a.id}"]
+subnets = ["${var.subnet_id}"]
+health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 3
+    interval = 30
+    target = "HTTP:8080/"
+  }
+  listener {
+    lb_port = 80
+    lb_protocol = "http"
+    instance_port = "8080"
+    instance_protocol = "http"
+  }
 }
