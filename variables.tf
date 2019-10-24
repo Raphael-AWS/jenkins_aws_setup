be8687a813149c5d90e246e24daaa23ccb4f8f66
Raphael Titus <56334419+Raphael-AWS@users.noreply.github.com>
2019-10-24 12:53:38 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,14 +5,38 @@ variable "aws_region" {
 variable "amis" {
     description = "AMIs by region"
     default = {
-        us-east-1 = "ami-0cfee17793b08a293" # ubuntu 14.04 LTS
+        us-east-1 = "ami-04b9e92b5572fa0d1" # ubuntu 14.04 LTS
     }
 }
-variable "aws_key_name" {
+variable "key_name" {
   description = "Key name for SSHing into EC2"
   default = "blockchain"
 }
 variable "aws_availability_zones" {
-  default     = "us-east-1a,us-east-1b,us-east-1c,us-east-1d"
+  default     = "eu-west-1a,us-east-1a,us-east-1b,us-east-1c,us-east-1d"
   description = "List of availability zones, use AWS CLI to find your "
+}
+variable "subnet_id" {
+  description = "subnet id of vpc"
+  default = "subnet-881a99a6"
+}
+variable "instance_type" {
+  description = "subnet id of vpc"
+  default = "t2.medium"
+}
+variable "security_group_id" {
+  description = "subnet id of vpc"
+  default = "sg-0e16de0c2453bce61"
+}
+variable "asg_min" {
+  description = "Min numbers of servers in ASG"
+  default     = "2"
+}
+variable "asg_max" {
+  description = "Max numbers of servers in ASG"
+  default     = "3"
+}
+variable "asg_desired" {
+  description = "Desired numbers of servers in ASG"
+  default     = "2"
 }
