5c68f4e84cc41e9dd50097a70e9ab9f1868cde5b
Raphael Titus <56334419+Raphael-AWS@users.noreply.github.com>
2019-10-24 13:06:36 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,134 +1,24 @@
 #!/bin/bash
-
-set -x
-
-function wait_for_jenkins()
-{
-  while (( 1 )); do
-      echo "waiting for Jenkins to launch on port [8080] ..."
-      
-      nc -zv 127.0.0.1 8080
-      if (( $? == 0 )); then
-          break
-      fi
-
-      sleep 10
-  done
-
-  echo "Jenkins launched"
-}
-
-function updating_jenkins_master_password ()
-{
-  cat > /tmp/jenkinsHash.py <<EOF
-import bcrypt
-import sys
-if not sys.argv[1]:
-  sys.exit(10)
-plaintext_pwd=sys.argv[1]
-encrypted_pwd=bcrypt.hashpw(sys.argv[1], bcrypt.gensalt(rounds=10, prefix=b"2a"))
-isCorrect=bcrypt.checkpw(plaintext_pwd, encrypted_pwd)
-if not isCorrect:
-  sys.exit(20);
-print "{}".format(encrypted_pwd)
-EOF
-
-  chmod +x /tmp/jenkinsHash.py
-  
-  # Wait till /var/lib/jenkins/users/admin* folder gets created
-  sleep 10
-
-  cd /var/lib/jenkins/users/admin*
-  pwd
-  while (( 1 )); do
-      echo "Waiting for Jenkins to generate admin user's config file ..."
-
-      if [[ -f "./config.xml" ]]; then
-          break
-      fi
-
-      sleep 10
-  done
-
-  echo "Admin config file created"
-
-  admin_password=$(python /tmp/jenkinsHash.py ${jenkins_admin_password} 2>&1)
-  
-  # Please do not remove alter quote as it keeps the hash syntax intact or else while substitution, $<character> will be replaced by null
-  xmlstarlet -q ed --inplace -u "/user/properties/hudson.security.HudsonPrivateSecurityRealm_-Details/passwordHash" -v '#jbcrypt:'"$admin_password" config.xml
-
-  # Restart
-  systemctl restart jenkins
-  sleep 10
-}
-
-function install_packages ()
-{
-
-  wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
-  rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
-  yum install -y jenkins
-
-  # firewall
-  #firewall-cmd --permanent --new-service=jenkins
-  #firewall-cmd --permanent --service=jenkins --set-short="Jenkins Service Ports"
-  #firewall-cmd --permanent --service=jenkins --set-description="Jenkins Service firewalld port exceptions"
-  #firewall-cmd --permanent --service=jenkins --add-port=8080/tcp
-  #firewall-cmd --permanent --add-service=jenkins
-  #firewall-cmd --zone=public --add-service=http --permanent
-  #firewall-cmd --reload
-  systemctl enable jenkins
-  systemctl restart jenkins
-  sleep 10
-}
-
-function configure_jenkins_server ()
-{
-  # Jenkins cli
-  echo "installing the Jenkins cli ..."
-  cp /var/cache/jenkins/war/WEB-INF/jenkins-cli.jar /var/lib/jenkins/jenkins-cli.jar
-
-  # Getting initial password
-  # PASSWORD=$(cat /var/lib/jenkins/secrets/initialAdminPassword)
-  PASSWORD="${jenkins_admin_password}"
-  sleep 10
-
-  jenkins_dir="/var/lib/jenkins"
-  plugins_dir="$jenkins_dir/plugins"
-
-  cd $jenkins_dir
-
-  # Open JNLP port
-  xmlstarlet -q ed --inplace -u "/hudson/slaveAgentPort" -v 33453 config.xml
-
-  cd $plugins_dir || { echo "unable to chdir to [$plugins_dir]"; exit 1; }
-
-  # List of plugins that are needed to be installed 
-  plugin_list="git-client git github-api github-oauth github MSBuild ssh-slaves workflow-aggregator ws-cleanup"
-
-  # remove existing plugins, if any ...
-  rm -rfv $plugin_list
-
-  for plugin in $plugin_list; do
-      echo "installing plugin [$plugin] ..."
-      java -jar $jenkins_dir/jenkins-cli.jar -s http://127.0.0.1:8080/ -auth admin:$PASSWORD install-plugin $plugin
-  done
-
-  # Restart jenkins after installing plugins
-  java -jar $jenkins_dir/jenkins-cli.jar -s http://127.0.0.1:8080 -auth admin:$PASSWORD safe-restart
-}
-
-### script starts here ###
-
-install_packages
-
-wait_for_jenkins
-
-updating_jenkins_master_password
-
-wait_for_jenkins
-
-configure_jenkins_server
-
-echo "Done"
-exit 0
+JENKINS_URL="${jenkins_url}"
+JENKINS_USERNAME="${jenkins_username}"
+JENKINS_PASSWORD="${jenkins_password}"
+TOKEN=$(curl -u $JENKINS_USERNAME:$JENKINS_PASSWORD ''$JENKINS_URL'/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)')
+INSTANCE_NAME=$(curl -s 169.254.169.254/latest/meta-data/local-hostname)
+INSTANCE_IP=$(curl -s 169.254.169.254/latest/meta-data/local-ipv4)
+JENKINS_CREDENTIALS_ID="${jenkins_credentials_id}"
+sleep 60
+curl -v -u $JENKINS_USERNAME:$JENKINS_PASSWORD -H "$TOKEN" -d 'script=
+import hudson.model.Node.Mode
+import hudson.slaves.*
+import jenkins.model.Jenkins
+import hudson.plugins.sshslaves.SSHLauncher
+DumbSlave dumb = new DumbSlave("'$INSTANCE_NAME'",
+"'$INSTANCE_NAME'",
+"/home/ec2-user",
+"3",
+Mode.NORMAL,
+"slaves",
+new SSHLauncher("'$INSTANCE_IP'", 22, SSHLauncher.lookupSystemCredentials("'$JENKINS_CREDENTIALS_ID'"), "", null, null, "", "", 60, 3, 15),
+RetentionStrategy.INSTANCE)
+Jenkins.instance.addNode(dumb)
+' $JENKINS_URL/script
